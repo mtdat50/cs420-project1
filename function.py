7968529781cc88b43d5e7cdf66489202bc698d97
Mai Tấn Đạt <maitandat50@gmail.com>
2023-11-27 20:59:28 +0700

    First commit. Main program outline.

--- function.py
+++ function.py
@@ -1,3 +1,33 @@
+UP = 0
+RIGHT = 1
+DOWN = 2
+LEFT = 3
+UPSTAIR = 4
+SAMESTAIR = 5
+DOWNSTAIR = 6
 
-def input(map):
-    
+R = {-1, 0, 1, 0}
+C = {0, 1, 0, -1}
+F = {1, 0, -1}
+
+
+def input():
+    TODO
+
+    #map[f][m][n] for f floors
+    return map
+
+#draw the floor where agent A1 is on
+def output(map, agents_coord): #coord is agents' current position
+    TODO
+
+
+#agent -> key/target/up/down stair ; up/down -> key/target/up/down stair ; key -> door/other key ; door -> key/target
+def convert2Graph(map): 
+    TODO
+    return #adjacent list g[n][...] = (adjacent vertex, path{UP, DOWN,...}) and list of agents, targets' index (which vertex if agent_i, which vertex is target_i)
+
+#find a path from a selected agent to its target
+def findPath(g, agent_index, target_index): #g = adjacent list
+    return #list of steps{UP DOWN LEFT RIGHT} 
+
--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 from function import *
+from time import sleep
+
+map = input()
+f = len(map)
+m = len(map[0])
+n = len(map[0][0])
+
+#scan thro the map and get the agents coordinate
+agents_coord = []
 
 
+g, agents, targets = convert2Graph(map)
+path = findPath(g, agent_index = agents[0], target_index = targets[0])
 
 
+for step in path:
+    if step < 4:
+        agents_coord[0][1] += R[step]
+        agents_coord[0][2] += C[step]
+    else:
+        agents_coord[0][0] += F[step - 4]
+
+    output(map, agents_coord=agents_coord)
+    sleep(0.5)
