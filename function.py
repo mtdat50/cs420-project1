ae35833d3eedc64a9929d12246cd0b4730a4765e
Mai Tấn Đạt <maitandat50@gmail.com>
2023-12-01 03:05:31 +0700

    findPath function is done. Issue: small path for each edge is wrong (convert2Graph function)

--- function.py
+++ function.py
@@ -1,4 +1,5 @@
 from collections import deque
+from queue import PriorityQueue
 
 UP = 0
 RIGHT = 1
@@ -10,6 +11,7 @@ UPLEFT = 4
 # R = {-1, 0, 1, 0}
 # C = {0, 1, 0, -1}
 # F = {1, 0, -1}
+
 
 
 def input(filepath):
@@ -26,15 +28,16 @@ def input(filepath):
                 f += 1
 
     with open(filepath, 'r') as filein:
-        m = int(filein.read())
-        n = int(filein.read())
+        m, n = filein.readline()[:-1].split(',')
+        m = int(m)
+        n = int(n)
         # map[f][m][n] for f floors, m rows, n columns
-        map = [[[(0, 0)] * n for i in range(m)] for j in range(f)]
+        map = [[[] * n for i in range(m)] for j in range(f)]
         
         for k in range(f):
             filein.readline()
             for i in range(m):
-                map[k][i] = filein.readline().split(',')
+                map[k][i] = filein.readline()[:-1].split(',')
                 for j in range(len(map[0][0])):
                     if map[k][i][j][0] == 'A':
                         agentCoord.append((map[k][i][j], k, i, j))
@@ -49,25 +52,31 @@ def input(filepath):
 def output(map, agents_coord): #list of agents' current position, [][1] = floor, [][2] = row, [][3] = column
     TODO
 
-def _tracePath(steppingMap, r, c, sourceR, sourceS):
+def _tracePath(steppingTrack, r, c, sourceR, sourceS):
     path = []
+    i = 0
     while r != sourceR and c != sourceS:
-        path.append(steppingMap[r][c])
-        r -= steppingMap[r][c][0]
-        c -= steppingMap[r][c][1]
+        i += 1
+        path.append(steppingTrack[r][c])
+        step = steppingTrack[r][c]
+        r -= step[0]
+        c -= step[1]
+
     
     path.reverse()
     return path
 
 def convert2Graph(map, agentCoord): 
-    g = [[]]
+    g = []
     vertexType = []
     objectCoord = []
+    objIndexList = {}
     for agent in agentCoord:
+        objIndexList[(agent[1], agent[2], agent[3])] = len(g)
         g.append([])
-        vertexType.append(agent[0])
+        vertexType.append(map[agent[1]][agent[2]][agent[3]])
         objectCoord.append((agent[1], agent[2], agent[3]))
-
+        
     f = len(map)
     m = len(map[0])
     n = len(map[0][0])
@@ -76,25 +85,31 @@ def convert2Graph(map, agentCoord):
             for j in range(n):
                 if map[k][i][j] != '0' and map[k][i][j] != '-1' and map[k][i][j][0] != 'A':
                     objectCoord.append((k, i, j))
+                    objIndexList[(k, i, j)] = len(g)
                     g.append([])
                     vertexType.append(map[k][i][j])
                     if map[k][i][j] == 'UP':
                         objectCoord.append((k + 1, i, j))
-                        g[-1].append((len(g), (0, 0)))
+                        objIndexList[(k + 1, i, j)] = len(g)
+                        g[-1].append((len(g), []))
                         g.append([])
                         vertexType.append('0')
                     if map[k][i][j] == 'DO':
                         objectCoord.append((k - 1, i, j))
-                        g[-1].append((len(g), (0, 0)))
+                        objIndexList[(k - 1, i, j)] = len(g)
+                        g[-1].append((len(g), []))
                         g.append([])
                         vertexType.append('0')
 
+
+
     #for every object, bfs at its floor
     for obj in objectCoord:
-        steppingMap = [[] * n for i in range(m)]
+        steppingTrack = [[(0, 0)] * n for i in range(m)]
         q = deque()
         q.append(obj)
-        curObjIndex = vertexType.index(map[obj[0]][obj[1]][obj[2]])
+        curObjIndex = objIndexList[(obj[0], obj[1], obj[2])]
+        steppingTrack[obj[1]][obj[2]] = (-10, -10)
 
         while len(q) != 0:
             curF = q[0][0]
@@ -102,27 +117,90 @@ def convert2Graph(map, agentCoord):
             curC = q[0][2]
             q.popleft()
 
+
             for i in range(-1, 2):
                 newR = curR + i
                 if newR < 0 or newR >= m:
                     continue
 
                 for j in range(-1, 2):
+                    if i == 0 and j == 0:
+                        continue
                     newC = curC + j
+
                     if newC < 0 or newC >= n or map[curF][newR][newC] == '-1' or map[curF][newR][newC][0] == 'A':
                         continue
                     if i != 0 and j != 0 and (map[curF][curR][newC] == '-1' or map[curF][newR][curC] == '-1'):
                         continue
+                    if steppingTrack[newR][newC] != (0, 0):
+                        continue
+                        
+                    steppingTrack[newR][newC] = (i, j)
                     
-                    steppingMap[curF][newR][newC] = (i, j)
-                    q.append((curF, newR, newC))
-
-                    if map[curF][newR][newC] != '0':
-                        newObjIndex = vertexType.index(map[curF][newR][newC])
-                        g[curObjIndex].append((newObjIndex, _tracePath(steppingMap, newR, newC, obj[1], obj[2])))
+                    if map[curF][newR][newC] == '0':
+                        q.append((curF, newR, newC))
+                    else:
+                        newObjIndex = objIndexList[(curF, newR, newC)]
+                        g[curObjIndex].append((newObjIndex, _tracePath(steppingTrack, newR, newC, obj[1], obj[2])))
     return g, vertexType
 
+def _tracePath2(prevEdge, g, agent_index, u, keys):
+    path = []
+    nodes = [u]
+    while u != (agent_index - 1) or keys != 0:
+        prevVertex = prevEdge[u][keys][0]
+        edgeIndex = prevEdge[u][keys][1]
+        path += (g[prevVertex][edgeIndex][1])
+        keys = prevEdge[u][keys][2]
+        u = prevVertex
+        nodes.append(u)
+
+    nodes.reverse()
+    path.reverse()
+    return path, nodes
+
+
 #find a path from a selected agent to its target
-def findPath(g, agent_index): #g = adjacent list
-    return #list of steps{deltaR, deltaC} 
+def findPath(g, vertexType, agent_index): #g = adjacent list
+    path = []
+    prevEdge = [[(0, 0, 0)] * 1024 for i in range(len(g))] #store vertex and edge index leading to the state
+    d = [[1e9] * 1024 for i in range(len(g))]
+    q = PriorityQueue()
+    q.put((0, agent_index - 1, 0))
+    d[agent_index - 1][0] = 0
+
+
+    while not q.empty():
+        temp = q.get()
+        d_u_keys = temp[0]
+        u = temp[1]
+        keys = temp[2]
+        if d[u][keys] != d_u_keys:
+            continue
+        
+
+        if vertexType[u][0] == 'T' and int(vertexType[u][1]) == agent_index:
+            path = _tracePath2(prevEdge, g, agent_index, u, keys)
+            break
+
+        
+        for i, edge in enumerate(g[u]):
+            if vertexType[edge[0]][0] == 'D':
+                if (keys >> (int(vertexType[edge[0]][1]) - 1)) & 1 == 0:
+                    continue
+
+            newKeys = keys
+            if vertexType[edge[0]][0] == 'K':
+                newKeys |= 1 << (int(vertexType[edge[0]][1]) - 1)
+            
+
+            if d[edge[0]][newKeys] <= d_u_keys + len(edge[1]):
+                continue
+
+            d[edge[0]][newKeys] = d_u_keys + len(edge[1])
+            prevEdge[edge[0]][newKeys] = (u, i, keys)
+            q.put((d[edge[0]][newKeys], edge[0], newKeys))
+
+    #list of steps{deltaR, deltaC}
+    return path
 
--- main.py
+++ main.py
@@ -1,25 +1,38 @@
 from function import *
 from time import sleep
 
-map, agentCoord = input()
+map, agentCoord = input('input.txt')
 f = len(map)
 m = len(map[0])
 n = len(map[0][0])
 
 
-
 g, vertexType= convert2Graph(map, agentCoord = agentCoord)
-path = findPath(g, agent_index = 1)
+for vertex, edgeList in enumerate(g):
+    print(vertex, vertexType[vertex], '====')
+    for edge in g[vertex]:
+        print(edge[0], vertexType[edge[0]])
+        print(edge[1])
+print(vertexType)
 
 
-for step in path:
-    agentCoord[0][2] += step[0]
-    agentCoord[0][3] += step[1]
+path, nodes = findPath(g, vertexType, agent_index = 1)
+# print(path)
+# for node in nodes:
+#     print(vertexType[node])
 
-    if map[agentCoord[0][1]][agentCoord[0][2]][agentCoord[0][3]] == 'UP':
-        agentCoord[0][1] += 1
-    elif map[agentCoord[0][1]][agentCoord[0][2]][agentCoord[0][3]] == 'DO':
-        agentCoord[0][1] -= 1
 
-    output(map, agents_coord = agentCoord)
-    sleep(0.5)
+# for step in path:
+#     agentCoord[0] = (agentCoord[0][0], agentCoord[0][1], agentCoord[0][2] + step[0], agentCoord[0][3])
+#     agentCoord[0] = (agentCoord[0][0], agentCoord[0][1], agentCoord[0][2], agentCoord[0][3] + step[1])
+
+#     if map[agentCoord[0][1]][agentCoord[0][2]][agentCoord[0][3]] == 'UP':
+#         agentCoord[0] = (agentCoord[0][0], agentCoord[0][1] + 1, agentCoord[0][2], agentCoord[0][3])
+#     elif map[agentCoord[0][1]][agentCoord[0][2]][agentCoord[0][3]] == 'DO':
+#         agentCoord[0] = (agentCoord[0][0], agentCoord[0][1] - 1, agentCoord[0][2], agentCoord[0][3])
+
+#     # output(map, agents_coord = agentCoord)
+#     print(agentCoord[0][2], agentCoord[0][3])
+#     if map[agentCoord[0][1]][agentCoord[0][2]][agentCoord[0][3]] != '0':
+#         print(map[agentCoord[0][1]][agentCoord[0][2]][agentCoord[0][3]])
+#     sleep(0.25)
